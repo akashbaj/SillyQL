0b863b2bea20a02125265994e91c2ea31d942377
akashbaj <akashbaj@umich.edu>
2020-03-12 17:31:13 -0400

    Initial Commit

---
+++ Project3/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Project3
+//
+//  Created by Akash on 3/12/20.
+//  Copyright © 2020 Akash Bajpai. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
